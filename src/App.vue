54e66f069bf0e0bb044c0bf2613f1f924383628d
huzhangshuan <1300647703@qq.com>
2017-09-26 20:37:26 +0800

    卷皮优选提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <!-- <img src="./assets/logo.png"> -->
+    <jpyx></jpyx>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Jpyx from './components/YouXuan'
 
 export default {
-  name: 'app',
+  name: 'Japx',
   components: {
-    Hello
+    Jpyx
   }
 }
 </script>
 
 <style>
-#app {
+#app{
+  width:100%;
+}
+*{
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
